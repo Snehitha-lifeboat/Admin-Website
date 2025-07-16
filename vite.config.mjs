ace3d585a6f59c714dec1c527a8c628c27b022eb
snehitha <snehitha@lifboat.co.in>
2025-07-16 15:09:39 +0530

    changes in vite

--- vite.config.mjs
+++ vite.config.mjs
@@ -35,7 +35,7 @@ export default defineConfig(({ mode }) => {
       port: PORT
     },
     base: API_URL,
-    //  base: './', 
+     base: './', 
     plugins: [react(), jsconfigPaths()]
   };
 });
